057e4f0c422652b26d13465b339c42235cd4ba33
StuRuby <xingya.shi@gmail.com>
2020-09-20 09:35:55 +0800

    feature:添加自定义过渡效果header

---
+++ lib/pages/job/job_detail.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+import 'package:boss_zhipin/widgets/sliver_job_detail_header.dart';
+
+class JobDetail extends StatefulWidget {
+  @override
+  _JobDetail createState() => new _JobDetail();
+}
+
+class _JobDetail extends State<JobDetail> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: null,
+    );
+  }
+}
