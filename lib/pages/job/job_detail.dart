e4129626f4afd8cf786b40cd7886e481004d90be
StuRuby <xingya.shi@gmail.com>
2020-10-06 01:51:17 +0800

    feature:添加地图依赖

--- lib/pages/job/job_detail.dart
+++ lib/pages/job/job_detail.dart
@@ -1,6 +1,7 @@
-import 'package:boss_zhipin/widgets/job_detail_content.dart';
 import 'package:flutter/material.dart';
 
+import 'package:boss_zhipin/widgets/job_detail_content.dart';
+import 'package:boss_zhipin/widgets/company_desc_intro.dart';
 import 'package:boss_zhipin/models/job.dart';
 import 'package:boss_zhipin/widgets/sliver_job_detail_header.dart';
 import 'package:boss_zhipin/widgets/job_publisher_intro.dart';
@@ -34,12 +35,20 @@ class _JobDetail extends State<JobDetail> {
                     (BuildContext context, int index) => Row(
                           children: <Widget>[
                             Expanded(
-                              child: JobDetailContent(),
+                              child: Column(
+                                children: [
+                                  JobPublisherIntro(),
+                                  JobDetailContent(
+                                    job: widget.job,
+                                  ),
+                                  CompanyDescIntro(),
+                                ],
+                              ),
                               flex: 1,
                             ),
                           ],
                         ),
-                    childCount: 2)),
+                    childCount: 1)),
           ],
         ),
       ),
